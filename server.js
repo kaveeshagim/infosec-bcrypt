0c32b4a24ee5411319c1dd1511617836396fe2c4
Kaveesha <kaveeshagw123456@gmail.com>
2025-05-27 13:14:55 +0530

    hash and compare passwords async

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(someOtherPlaintextPassword, saltRounds, (err, hash) => {
+  console.log("res:", hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    /*res == true or false*/
+    console.log("res:", res);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
